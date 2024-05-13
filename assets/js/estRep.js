f6d31a946ff2ab9cfe946db1eef282b7e77a4c20
unknown <zenagsuma5@gmail.com>
2024-05-13 11:23:54 -0300

    Finalización de estRep e inicio con trangDe

---
+++ assets/js/estRep.js
@@ -0,0 +1,9 @@
+let nr;
+for (nr = 0; nr <=10; nr ++) {
+    console.log(nr);
+    if (nr % 2 === 0) {
+        console.log("El número es par");
+    } else if (nr % 1 === 0) {
+        console.log("El número es impar");
+    }
+}
